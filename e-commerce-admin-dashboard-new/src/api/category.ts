8fae9afe57595400a1ef3a0da24fdd136ae85a9b
Aayush Pradhan <muma5581@gmail.com>
2025-04-28 22:54:12 +0545

    build files

---
+++ e-commerce-admin-dashboard-new/src/api/category.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import api from "@/axios/api.axios";
+
+export const getAllCategories = async() => {
+    try{
+        const response = await api.get('/category')
+        return response?.data
+
+    }catch(error:any){
+        throw error?.response?.data; 
+    }
+}
+
+export const deleteCategory = async(id:string) => {
+    try{
+        const response = await api.delete(`/category/${id}`)
+        return response?.data
+    } catch(error:any) {
+        throw error?.response?.data;
+    }
+}
+
+export const createNewCategory = async() => {
+
+}
